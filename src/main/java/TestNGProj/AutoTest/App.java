30274cc41d433100745a268c045cd3451a19d1a0
xjz10 <xjz900@sohu.com>
2019-05-14 17:31:05 +0800

    第一次提交

---
+++ src/main/java/TestNGProj/AutoTest/App.java
@@ -0,0 +1,13 @@
+package TestNGProj.AutoTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
