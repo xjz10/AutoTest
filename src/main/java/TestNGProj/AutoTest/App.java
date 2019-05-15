41e61a0c6935a897a1271d7b7ba19359a4aa805b
xjz10 <xjz900@sohu.com>
2019-05-15 15:03:05 +0800

    第二次提交

--- src/main/java/TestNGProj/AutoTest/App.java
+++ src/main/java/TestNGProj/AutoTest/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" ); 
-        System.out.println( "First Project Hello World!" );
+        System.out.println( "First Project Hello World!" );  
+        System.out.println( "second Project Hello World!" );
     }
 }
